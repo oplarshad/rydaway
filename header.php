1046bb336eaba1723f8fc7818534a6a5cc6642a8
Ryder <dev@ryderdamen.com>
2018-02-18 14:19:58 -0500

    Building mobile menu

--- header.php
+++ header.php
@@ -17,13 +17,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="theme-color" content="#1a348b">
-	
-	<!-- Styles & Scripts -->
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-<!-- 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/r-logo-font/styles.css">
-	<script src="<?php echo get_template_directory_uri(); ?>/js/pace.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
 
 	
 	<!--
@@ -63,8 +58,112 @@
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'rydaway' ); ?></a>
 		<header id="masthead" class="site-header">
 			
-			<!--Navigation -->
-			<div class="site-branding">
+			<!--Mobile Navigation -->
+			<div class="mobile-menu">
+				<div class="mobile-menu-logo">
+					<h1 class="mobile-site-title">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</h1>
+				</div>
+				<div class="mobile-menu-button" id="mobile-menu-button">
+					<i class="fas fa-bars mobile-menu-icon" id="mobile-menu-button-icon"></i>
+				</div>
+				<div class="mobile-menu-search" id="mobile-menu-search">
+					<i class="fas fa-search mobile-menu-icon" id="mobile-menu-search-icon"></i>
+				</div>
+			</div>
+			<div class="mobile-search-bar" id="mobile-search-bar">
+				<form role="search" method="get" action="https://www.rydaway.com/" name="pageSearch" id="mainSearchForm">
+					<input type="search" class="mobile-search-bar-input" id="mobileSearchInput" placeholder="Looking for something?" name="s">
+				</form>
+				</input>
+			</div>
+			<script>
+				$(document).ready(function() {
+					
+					var menuOpen = false;
+					var searchOpen = false;
+					
+					function openMenu() {
+						document.getElementById('mobile-menu-button').style.backgroundColor = '#f8f8f8';
+						document.getElementById('mobile-menu-button-icon').style.color = '#0fb8ad';
+						document.getElementById('main-nav').style.display = 'block';
+						document.getElementById('masthead').style.background = '#1a348b';
+						menuOpen = true;
+					}
+					
+					function closeMenu() {
+						document.getElementById('mobile-menu-button').style.backgroundColor = 'transparent';
+						document.getElementById('mobile-menu-button-icon').style.color = '#fff';
+						document.getElementById('main-nav').style.display = 'none';
+						document.getElementById('masthead').style.background = 'linear-gradient(141deg, #0fb8ad 0%, #1fc8db 51%, #2cb5e8 75%)';
+						menuOpen = false;
+					}
+					
+					function openSearch() {
+						document.getElementById('mobile-menu-search').style.backgroundColor = '#f8f8f8';
+						document.getElementById('mobile-menu-search-icon').style.color = '#0fb8ad';
+						document.getElementById('mobile-search-bar').style.display = 'block';
+						searchOpen = true;
+					}
+					
+					function closeSearch() {
+						document.getElementById('mobile-menu-search').style.backgroundColor = 'transparent';
+						document.getElementById('mobile-menu-search-icon').style.color = '#fff';
+						document.getElementById('mobile-search-bar').style.display = 'none';
+						searchOpen = false;
+					}
+					
+					function resetHeader() {
+						document.getElementById('main-nav').style.display = 'block';
+					}
+					
+					// Mobile Search Bar Logic
+					$(document).ready(function(){
+									var originalSearchQuery = document.getElementById("mobileSearchInput").value;
+								    $("#searchBarHeader").focusout(function(){
+								        if (document.getElementById("mobileSearchInput").value != originalSearchQuery) {
+									        // Submit the search
+									        document.getElementById('mainSearchForm').submit();
+								        }
+								    });
+								});
+					
+					// Clickable Logic
+				    $('#mobile-menu-button').on('click', function() {
+					    if (!menuOpen) {
+						    openMenu()
+						    closeSearch()
+					    }
+					    else { // The menu is already open. Close it.
+						    closeMenu()
+					    }
+				    });
+				    
+				    $('#mobile-menu-search').on('click', function() {
+					    if (!searchOpen) { // The search is not open, open it.
+						    openSearch()
+						    closeMenu()
+					    }
+					    else { // The search is already open. Close it.
+						    closeSearch()
+					    }
+				    });
+					
+					// Resizing Logic
+					 $(window).resize(function() {
+						 if ($(window).width() > 768) {
+						 	resetHeader()
+						 }
+						 if ($(window).width() < 768) {
+						 	closeMenu()
+						 }
+						});
+				});
+			</script>
+			
+			<!-- Regular Navigation -->
+			<div class="site-branding" id="main-nav">
 				<div class="header-content">
 					<div class="logo-container">
 						<h1 class="site-title">
