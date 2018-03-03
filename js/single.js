75f7020579d90858830e6fbad5f817ca2c6bd271
Ryder <dev@ryderdamen.com>
2018-03-03 12:37:01 -0500

    cleanup

--- js/single.js
+++ js/single.js
@@ -1,11 +1,4 @@
 $(function(){ // On Document Ready
-	
 	// Slide the plane up
 	$('#planeTop').animate({ bottom: '25px' }, 1000);
-/*
-	
-	setTimeout(function() {
-		$('#planeTop').animate({ bottom: '25px' }, 1500);
-	}, 500);
-*/
 });
